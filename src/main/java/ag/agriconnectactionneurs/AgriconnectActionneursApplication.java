999b6b5db8e0b5f0b681c8416cf3860cdded98cd
PAN <paularthur@navaud.fr>
2024-06-11 16:51:07 +0200

    Add the config global

--- src/main/java/ag/agriconnectactionneurs/AgriconnectActionneursApplication.java
+++ src/main/java/ag/agriconnectactionneurs/AgriconnectActionneursApplication.java
@@ -2,7 +2,9 @@ package ag.agriconnectactionneurs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AgriconnectActionneursApplication {
 
