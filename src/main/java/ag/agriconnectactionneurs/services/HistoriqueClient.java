8ab53a985a4afe65a15504d0908936e36618d677
Martin Billard <martin.billard@toulouse.miage.fr>
2024-06-17 22:16:01 +0200

    Refactor routes des FeignClient pour correspondre à la norme

--- src/main/java/ag/agriconnectactionneurs/services/HistoriqueClient.java
+++ src/main/java/ag/agriconnectactionneurs/services/HistoriqueClient.java
@@ -4,11 +4,13 @@ import ag.agriconnectactionneurs.entities.Historique;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @FeignClient("achistoriques")
 public interface HistoriqueClient {
 
-    @PostMapping("/api/historiques")
+    @RequestMapping(value = "/api/historiques", method = RequestMethod.POST)
     Historique createHistorique(@RequestBody Historique historique);
 }
